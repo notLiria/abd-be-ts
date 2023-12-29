c6ef15fac041f71f467c1c6b3624e972e38b304f
Liria <hudashing+github@gmail.com>
2023-12-29 15:21:01 -0500

    Fix addShafts for new API

--- src/controllers/arrow-shaft.controller.ts
+++ src/controllers/arrow-shaft.controller.ts
@@ -11,12 +11,15 @@ import {
   getModelSchemaRef,
   param,
   post,
+  put,
   requestBody,
   response,
 } from '@loopback/rest';
+import debugFactory from 'debug';
 import {ArrowShaft} from '../models';
 import {ArrowShaftRepository} from '../repositories';
 
+const debug = debugFactory('arrowShaftController')
 export class ArrowShaftController {
   constructor(
     @repository(ArrowShaftRepository)
@@ -75,5 +78,42 @@ export class ArrowShaftController {
     @param.filter(ArrowShaft) filter?: Filter<ArrowShaft>,
   ): Promise<ArrowShaft[]> {
     return this.arrowShaftRepository.find(filter);
+  }
+
+  @authenticate('jwt')
+  @put('/arrow-shafts')
+  @response(200, {
+    description: 'ArrowShaft model instance',
+    content: {'application/json': {schema: getModelSchemaRef(ArrowShaft)}},
+  })
+  async update(
+    @requestBody({
+      content: {
+        'application/json': {
+          schema: {
+            type: 'array',
+            items: getModelSchemaRef(ArrowShaft, {
+              title: 'NewArrowShaft',
+              exclude: ['shaftId'],
+            }),
+          },
+        },
+      },
+    })
+    arrowShafts: Omit<ArrowShaft, 'shaft_id'>[],
+  ): Promise<void>{//Promise<ArrowShaft[]> {
+    for (const shaft of arrowShafts) {
+      const existingShaft = await this.arrowShaftRepository.findOne({
+        where: {manufacturer: shaft.manufacturer, model: shaft.model, spine: shaft.spine}
+      })
+      if (existingShaft) {
+        await this.arrowShaftRepository.updateById(existingShaft.shaftId, shaft)
+        debug(`Shaft already exists, updating`)
+      }
+      else {
+        await this.arrowShaftRepository.create(shaft)
+        debug(`Shaft does not exist, creating`)
+      }
+    }
   }
 }
